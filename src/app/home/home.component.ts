f352c8ab282cb94b6d332628cc61a9dd4fc72714
Scott VandenToorn <svandentoorn@Scotts-MacBook-Pro.local>
2019-07-13 23:24:24 -0400

    Updated icons to work in safari

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,11 +7,16 @@ import { MatSnackBar } from '@angular/material';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  chrome: any;
+  safari: any;
 
   constructor(private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     window.scrollTo(0, 0);
+    this.chrome = navigator.userAgent.indexOf('Chrome') > -1;
+    this.safari = navigator.userAgent.indexOf('Safari') > -1;
+    if ((this.chrome) && (this.safari)) this.safari = false;
   }
 
   openSnackBar(message: string, action = 'Dismiss') {
