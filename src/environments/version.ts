dc61242e8608d7797fc5c655da4519dfcd0483ae
Scott VandenToorn <svandentoorn@Scotts-MacBook-Pro.local>
2019-07-16 19:59:55 -0400

    Added build version info for build info page

---
+++ src/environments/version.ts
@@ -0,0 +1,15 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "8a1bcc0-dirty",
+    "hash": "8a1bcc0",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "8a1bcc0-dirty",
+    "semverString": null,
+    "version": "0.0.0",
+    "date": "July 16, 2019, 7:49 PM"
+};
+/* tslint:enable */
