5327db4bf6879423612973a1da07544b2a1887e1
Scott VandenToorn <vandents@mail.gvsu.edu>
2019-07-18 22:56:02 -0400

    Text now drops below avi in first card on home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 import { Title } from '@angular/platform-browser';
 
@@ -8,11 +8,13 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  screenWidth: number;
   chrome: any;
   safari: any;
 
   constructor(private title: Title, private snackBar: MatSnackBar) {
     this.title.setTitle('Scott VandenToorn - Home');
+    this.getScreenSize();
   }
 
   ngOnInit() {
@@ -20,6 +22,16 @@ export class HomeComponent implements OnInit {
     this.chrome = navigator.userAgent.indexOf('Chrome') > -1;
     this.safari = navigator.userAgent.indexOf('Safari') > -1;
     if ((this.chrome) && (this.safari)) this.safari = false;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.screenWidth = window.innerWidth;
+  }
+
+  isSmallPhone() {
+    if (this.screenWidth > 400) return false;
+    return true;
   }
 
   openSnackBar(message: string, action = 'Dismiss') {
