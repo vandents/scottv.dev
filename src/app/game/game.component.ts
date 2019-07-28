3955109e16e41bf6587d2ce5b0e144d28ae11770
Scott VandenToorn <vandents@mail.gvsu.edu>
2019-07-28 00:08:26 -0400

    Added animations

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -13,6 +13,21 @@ enum Player {
 }
 
 /**
+ * Left slants this way: \
+ * Right slants this way: /
+ */
+enum WinPos {
+  row1 = 1,
+  row2,
+  row3,
+  col1,
+  col2,
+  col3,
+  left,
+  right
+}
+
+/**
  * A little something I hacked together.
  * One thing to note is that the Mr. Roboto hijacks Player O's board array
  * when the user selects to play against him. Just a little heads up for anyone
@@ -25,12 +40,21 @@ enum Player {
   animations: [
     trigger('fastFade', [
       state('in', style({ opacity: 1 })),
-      transition(':enter', [style({ opacity: 0.5 }), animate(300)]),
-      transition(':leave', animate(350, style({ opacity: 0.3, color: 'rgb(38, 38, 38)' })))
+      transition(':enter', [style({ opacity: 0.5 }), animate(300)])
     ]),
     trigger('mediumFade', [
       state('in', style({ opacity: 1 })),
-      transition(':enter', [style({ opacity: 0.2 }), animate(450)]),
+      transition(':enter', [style({ opacity: 0.2 }), animate(450)])
+    ]),
+    trigger('blue', [
+      state('in', style({ color: '#007bff' })),
+      transition(':enter', [style({ opacity: 0.5 }), animate(400)]),
+      transition(':leave', animate(350, style({ opacity: 0.3 })))
+    ]),
+    trigger('grey', [
+      state('in', style({ color: 'rgb(66, 66, 66' })),
+      transition(':enter', [style({ opacity: 0.5 }), animate(400)]),
+      transition(':leave', animate(350, style({ opacity: 0.3 })))
     ])
   ]
 })
@@ -44,6 +68,9 @@ export class GameComponent implements OnInit {
   winner: Player;
   /** True if user is playing against Mr. Roboto */
   isRobot: boolean;
+  /** Positions of icons that should turn grey on win condition */
+  greyArray: boolean[];
+  gameOver: boolean;
 
   constructor(
     private dialog: MatDialog,
@@ -66,6 +93,8 @@ export class GameComponent implements OnInit {
     this.oArray = [[false, false, false], [false, false, false], [false, false, false]];
     this.whosTurn = Math.random() < 0.5;
     this.numTurns = 0;
+    this.greyArray = [false, false, false, false, false, false, false, false, false];
+    this.gameOver = false;
   }
 
   /** Initializes board. If it's Mr. Robot's turn after init robotMove() is called */
@@ -78,6 +107,7 @@ export class GameComponent implements OnInit {
     }
   }
 
+  /** Initializes board, then opens choose competitor dialog */
   changeOpponent() {
     this.initBoard();
     this.openChooseCompetitorDialog();
@@ -85,7 +115,7 @@ export class GameComponent implements OnInit {
 
   /** Main game handler */
   squareClicked(row: number, column: number) {
-    if (this.isRobotTurn()) return;
+    if (this.isRobotTurn() || this.gameOver) return;
     if (this.whosTurn) {
       if (this.oArray[row][column] == false) {
         this.xArray[row][column] = true;
@@ -124,57 +154,131 @@ export class GameComponent implements OnInit {
    * @returns true if win condition is found
    */
   checkForWinner(): boolean {
-    for (let i = 0; i <= 2; i++) {
-      if (this.xArray[i][0] && this.xArray[i][1] && this.xArray[i][2]) { this.winner = Player.x; return true; }
-    }
-    for (let i = 0; i <= 2; i++) {
-      if (this.xArray[0][i] && this.xArray[1][i] && this.xArray[2][i]) { this.winner = Player.x; return true; }
-    }
-    if (this.xArray[0][0] && this.xArray[1][1] && this.xArray[2][2]) { this.winner = Player.x; return true; }
-    if (this.xArray[0][2] && this.xArray[1][1] && this.xArray[2][0]) { this.winner = Player.x; return true; }
+    if (this.numTurns < 5) return false;  // Can't have win condition if less than 5 turns have been played
 
-    if (this.oArray[0][0] && this.oArray[0][1] && this.oArray[0][2]) {
-      if (this.isRobot) this.winner = Player.robot;
-      else  this.winner = Player.o;
+    /**** Check X array ****/
+    // Check rows
+    for (let i = 0; i <= 2; i++) {
+      if (this.xArray[i][0] && this.xArray[i][1] && this.xArray[i][2]) {
+        this.winner = Player.x;
+        if (i === 0) this.setWinAnimationArray(WinPos.row1);        // First row
+        else if (i === 1) this.setWinAnimationArray(WinPos.row2);   // Second row
+        else if (i === 2) this.setWinAnimationArray(WinPos.row3);   // Third row
+        return true;
+      }
+    }
+    // Check columns
+    for (let i = 0; i <= 2; i++) {
+      if (this.xArray[0][i] && this.xArray[1][i] && this.xArray[2][i]) {
+        this.winner = Player.x;
+        if (i === 0) this.setWinAnimationArray(WinPos.col1);        // First column
+        else if (i === 1) this.setWinAnimationArray(WinPos.col2);   // Second column
+        else if (i === 2) this.setWinAnimationArray(WinPos.col3);   // Third column
+        return true;
+      }
+    }
+    // Check diagonal left
+    if (this.xArray[0][0] && this.xArray[1][1] && this.xArray[2][2]) {
+      this.winner = Player.x;
+      this.setWinAnimationArray(WinPos.left);
       return true;
     }
-    if (this.oArray[1][0] && this.oArray[1][1] && this.oArray[1][2]) {
-      if (this.isRobot) this.winner = Player.robot;
-      else this.winner = Player.o;
+    // Check diagonal right
+    if (this.xArray[0][2] && this.xArray[1][1] && this.xArray[2][0]) {
+      this.winner = Player.x;
+      this.setWinAnimationArray(WinPos.right);
       return true;
     }
-    if (this.oArray[2][0] && this.oArray[2][1] && this.oArray[2][2]) {
-      if (this.isRobot) this.winner = Player.robot;
-      else this.winner = Player.o;
-      return true;
+
+    /**** Check O array ****/
+    // Check rows
+    for (let i = 0; i <= 2; i++) {
+      if (this.oArray[i][0] && this.oArray[i][1] && this.oArray[i][2]) {
+        this.isRobot ? this.winner = Player.robot : this.winner = Player.o;
+        if (i === 0) this.setWinAnimationArray(WinPos.row1);        // First row
+        else if (i === 1) this.setWinAnimationArray(WinPos.row2);   // Second row
+        else if (i === 2) this.setWinAnimationArray(WinPos.row3);   // Third row
+        return true;
+      }
     }
-    if (this.oArray[0][0] && this.oArray[1][0] && this.oArray[2][0]) {
-      if (this.isRobot) this.winner = Player.robot;
-      else this.winner = Player.o;
-      return true;
+    // Check columns
+    for (let i = 0; i <= 2; i++) {
+      if (this.oArray[0][i] && this.oArray[1][i] && this.oArray[2][i]) {
+        this.isRobot ? this.winner = Player.robot : this.winner = Player.o;
+        if (i === 0) this.setWinAnimationArray(WinPos.col1);        // First column
+        else if (i === 1) this.setWinAnimationArray(WinPos.col2);   // Second column
+        else if (i === 2) this.setWinAnimationArray(WinPos.col3);   // Third column
+        return true;
+      }
     }
-    if (this.oArray[0][1] && this.oArray[1][1] && this.oArray[2][1]) {
-      if (this.isRobot) this.winner = Player.robot;
-      else this.winner = Player.o;
-      return true;
-    }
-    if (this.oArray[0][2] && this.oArray[1][2] && this.oArray[2][2]) {
-      if (this.isRobot) this.winner = Player.robot;
-      else this.winner = Player.o;
-      return true;
-    }
+    // Check diagonal left
     if (this.oArray[0][0] && this.oArray[1][1] && this.oArray[2][2]) {
-      if (this.isRobot) this.winner = Player.robot;
-      else this.winner = Player.o;
+      this.isRobot ? this.winner = Player.robot : this.winner = Player.o;
+      this.setWinAnimationArray(WinPos.left);
       return true;
     }
+    // Check diagonal right
     if (this.oArray[0][2] && this.oArray[1][1] && this.oArray[2][0]) {
-      if (this.isRobot) this.winner = Player.robot;
-      else this.winner = Player.o;
+      this.isRobot ? this.winner = Player.robot : this.winner = Player.o;
+      this.setWinAnimationArray(WinPos.right);
       return true;
     }
 
     return false;
+  }
+
+  /**
+   * Sets the the board positions in fadeArray[] that should fade
+   * Positions that are left false will turn blue
+   * @param winPosition row, col, or slant of win condition
+   */
+  setWinAnimationArray(winPosition: WinPos) {
+    switch (winPosition) {
+      case WinPos.row1: { // Win positions: 1, 2, 3
+        for (let i = 3; i <= 8; i++) this.greyArray[i] = true;
+        break;
+      }
+      case WinPos.row2: { // Win positions: 3, 4, 5
+        for (let i = 0; i <= 2; i++) this.greyArray[i] = true;
+        for (let i = 6; i <= 8; i++) this.greyArray[i] = true;
+        break;
+      }
+      case WinPos.row3: { // Win positions: 6, 7, 8
+        for (let i = 0; i <= 5; i++) this.greyArray[i] = true;
+        break;
+      }
+      case WinPos.col1: { // Win positions: 0, 3, 6
+        for (let i = 1; i <= 8; i++) {
+          if (i !== 3 && i !== 6) this.greyArray[i] = true; // Don't set winning column
+        }
+        break;
+      }
+      case WinPos.col2: { // Win positions: 1, 4, 7
+        for (let i = 0; i <= 8; i++) {
+          if (i !== 1 && i !== 4 && i !== 7) this.greyArray[i] = true; // Don't set winning column
+        }
+        break;
+      }
+      case WinPos.col3: { // Win positions: 2, 5, 8
+        for (let i = 0; i <= 7; i++) {
+          if (i !== 2 && i !== 5) this.greyArray[i] = true; // Don't set winning column
+        }
+        break;
+      }
+      case WinPos.left: { // Win positions: 0, 4, 8 (\)
+        for (let i = 1; i <= 3; i++) this.greyArray[i] = true;
+        for (let i = 5; i <= 7; i++) this.greyArray[i] = true;
+        break;
+      }
+      case WinPos.left: { // Win positions: 2, 4, 6 (/)
+        for (let i = 0; i <= 8; i++) {
+          if (i !== 2 && (i !== 4 && i !== 6)) this.greyArray[i] = true; // Don't set winning column
+        }
+        break;
+      }
+    }
+
+    this.gameOver = true;
   }
 
   /** Main handler for Mr. Roboto's turn */
@@ -528,18 +632,20 @@ export class GameComponent implements OnInit {
 
   /** Opens GameWinDialog */
   openGameWinDialog() {
-    const dialogRef = this.dialog.open(
-      GameWinDialogComponent, {
-        data: {
-          winner: this.winner,
-          isRobot: this.isRobot
+    setTimeout(() => {
+      const dialogRef = this.dialog.open(
+        GameWinDialogComponent, {
+          data: {
+            winner: this.winner,
+            isRobot: this.isRobot
+          }
         }
-      }
-    );
+      );
 
-    dialogRef.afterClosed().subscribe(result => {
-      this.resetBoard();
-    });
+      dialogRef.afterClosed().subscribe(() => {
+        this.resetBoard();
+      });
+    }, 925);
   }
 
   /** Opens ChooseCompetitorDialog */
