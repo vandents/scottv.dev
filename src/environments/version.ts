fa2820509aba372ba164a673c23e319617aa12d4
Scott VandenToorn <vandents@mail.gvsu.edu>
2019-09-04 15:07:28 -0400

    clean up uncommitted files

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,14 +2,14 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "99381b2-dirty",
-    "hash": "99381b2",
+    "raw": "51ee229-dirty",
+    "hash": "51ee229",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "99381b2-dirty",
+    "suffix": "51ee229-dirty",
     "semverString": null,
     "version": "0.0.0",
-    "date": "August 1, 2019, 11:04 PM"
+    "date": "September 3, 2019, 5:43 PM"
 };
 /* tslint:enable */
