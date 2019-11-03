063a9992a0ae1bd1323c863b86b54629a38eedb0
vandents <vandents@mail.gvsu.edu>
2019-11-03 17:55:29 -0500

    Bump dependencies. Update resume.

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,14 +2,14 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "51ee229-dirty",
-    "hash": "51ee229",
+    "raw": "09043f6-dirty",
+    "hash": "09043f6",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "51ee229-dirty",
+    "suffix": "09043f6-dirty",
     "semverString": null,
     "version": "0.0.0",
-    "date": "September 3, 2019, 5:43 PM"
+    "date": "November 3, 2019, 5:28 PM"
 };
 /* tslint:enable */
