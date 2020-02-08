048d1f92e822670cb9794d3736281fbe477ee527
vandents <svandentoorn@gmail.com>
2020-02-07 20:06:39 -0500

    Remove thin padding for algorithms page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
-import { BrowserService } from './services/browser-service/browser.service';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -10,22 +8,10 @@ import { Subscription } from 'rxjs';
 })
 export class AppComponent implements OnInit, OnDestroy {
   widthSub: Subscription;
-  containerClass: string;
 
-  constructor(
-    private router: Router,
-    private browser: BrowserService
-  ) {
-    this.containerClass = this.browser.isScreen500() ?
-      'container' : 'thin-container';
-  }
+  constructor() { }
 
   ngOnInit() {
-    // Apply thin container when viewing algorithms on mobile
-    this.widthSub = this.browser.widthChanges.subscribe(width => {
-      if (width <= 500 && this.router.url.includes('algorithms')) this.containerClass = 'thin-container';
-      else this.containerClass = 'container';
-    });
   }
 
   ngOnDestroy() {
