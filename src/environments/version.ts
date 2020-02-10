2c7f97e39def0edbf7a4d426738965b55c924917
vandents <svandentoorn@gmail.com>
2020-02-10 13:20:24 -0500

    Fix MATLAB capitalization

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,15 +1,15 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "5386822",
-    "hash": "5386822",
+    "dirty": true,
+    "raw": "0f99e27-dirty",
+    "hash": "0f99e27",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "5386822",
+    "suffix": "0f99e27-dirty",
     "semverString": null,
     "version": "0.0.0",
-    "date": "February 7, 2020, 8:48 PM"
+    "date": "February 10, 2020, 1:19 PM"
 };
 /* tslint:enable */
