48490b008da2aac24d1415585e0c8ebac2474344
vandents <svandentoorn@gmail.com>
2020-03-19 22:05:20 -0400

    Make ripple on chips work correctly in Safari

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,15 +1,15 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "36366ca-dirty",
-    "hash": "36366ca",
+    "dirty": false,
+    "raw": "d42469f",
+    "hash": "d42469f",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "36366ca-dirty",
+    "suffix": "d42469f",
     "semverString": null,
     "version": "0.0.0",
-    "date": "February 23, 2020, 8:49 PM"
+    "date": "March 11, 2020, 6:30 AM"
 };
 /* tslint:enable */
