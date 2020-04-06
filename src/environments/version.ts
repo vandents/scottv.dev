9c19204ba01fecd83bb1cbd52550298f7b39d785
vandents <svandentoorn@gmail.com>
2020-04-05 21:36:33 -0400

    Build project

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,14 +2,14 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": false,
-    "raw": "8b2f999",
-    "hash": "8b2f999",
+    "raw": "b17b7f3",
+    "hash": "b17b7f3",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "8b2f999",
+    "suffix": "b17b7f3",
     "semverString": null,
     "version": "0.0.0",
-    "date": "April 5, 2020, 9:25 PM"
+    "date": "April 5, 2020, 9:34 PM"
 };
 /* tslint:enable */
