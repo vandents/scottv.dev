e85e8fbcf6e475df4219de9cabed62c0890acf45
vandents <svandentoorn@gmail.com>
2024-03-12 15:56:24 -0400

    Create lazy load image directive

---
+++ src/app/directives/lazy-img-directive/lazy-img.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef } from '@angular/core';
+
+/**
+ * Directive that applies the lazy loading strategy to all images except
+ * those that are displayed inside a carousel
+ */
+@Directive({ selector: 'img' })
+export class LazyImgDirective {
+  constructor({ nativeElement }: ElementRef<HTMLImageElement>) {
+    const supports = 'loading' in HTMLImageElement.prototype;
+
+    if (supports && !nativeElement.classList.contains('d-block')) {
+      nativeElement.setAttribute('loading', 'lazy');
+    }
+  }
+}
