2f62ee201fdb0b9f01eeea0bda32fa4d6ee9b41f
vandents <svandentoorn@gmail.com>
2024-03-13 10:59:53 -0400

    Write to production environment file

--- src/set-env.ts
+++ src/set-env.ts
@@ -1,3 +1,13 @@
+/** Initialize environment.prod.ts with secrets */
+
+const fs = require('fs');
+
 let environment = process.argv[2];
 
-console.log('environment', environment);
+if (environment) {
+  // Write to production environment file
+  fs.writeFile('./src/environments/environment.prod.ts', environment, (err: any) => {
+    if (err) console.error(err);
+    console.log('Output generated at ./src/environments/environment.prod.ts');
+  });
+}
