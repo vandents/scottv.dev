c52acf80584d0d936a0ba8019a2b2b65dcbb676f
vandents <svandentoorn@gmail.com>
2024-03-14 16:34:38 -0400

    Remove unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule               } from '@angular/core';
-import { CommonModule           } from '@angular/common';
 import { Routes, RouterModule   } from '@angular/router';
 import { HomeComponent          } from '@pages/home/home.component';
 import { ProjectsComponent      } from '@pages/projects/projects.component';
