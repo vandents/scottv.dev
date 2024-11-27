354dbf0e808db8eea44ad564f49a50f737a8a67b
vandents <svandentoorn@gmail.com>
2024-11-27 15:16:45 -0500

    Update version info

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,14 +2,14 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "781d994-dirty",
-    "hash": "781d994",
+    "raw": "24806b2-dirty",
+    "hash": "24806b2",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "781d994-dirty",
+    "suffix": "24806b2-dirty",
     "semverString": null,
     "version": "0.0.0",
-    "date": "October 8, 2024 at 10:57 AM"
+    "date": "November 27, 2024 at 3:03 PM"
 };
 /* tslint:enable */
